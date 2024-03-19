7590adea7bfcd907b2ea05b232d507754b5f86b9
oyangor <robertanthonyoyango@gmail.com>
2024-03-19 13:17:36 +0300

    change1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
             className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
-            <li className="text-xl w-full my-2 font-bold"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
+            <li className="text-xl w-full my-2 font-bold" onClick={() => setToggleMenu(false)} ><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
             {data.map(
               (item, index) => <NavBarItem key={item + index} item={item} classprops="my-2 text-lg" />,
             )}
